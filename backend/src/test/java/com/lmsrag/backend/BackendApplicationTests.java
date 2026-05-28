5fd81a84ca59f977e6a351b7443e51380269fe5c
Trương Mỹ Tâm <TamTM.B21DVCN143@stu.ptit.edu.vn>
2026-05-28 23:01:09 +0700

    feat(backend): initialize spring boot maven project

---
+++ backend/src/test/java/com/lmsrag/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmsrag.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
